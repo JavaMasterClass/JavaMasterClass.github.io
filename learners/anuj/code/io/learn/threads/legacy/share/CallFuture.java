5b114158d61ff6fff629b13eac95b620595b7029
Anuj Jain <javamasterclass2021@gmail.com>
2021-01-19 19:18:06 +0530

    Run tasks in thread with Callable

---
+++ learners/anuj/code/io/learn/threads/legacy/share/CallFuture.java
@@ -0,0 +1,43 @@
+package io.learn.threads.legacy.share;
+
+import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+/**
+ * @author Run a task using Callable<T> returning a result
+ */
+public class CallFuture {
+
+    public static void main(String[] args) {
+
+        ExecutorService executor = Executors.newCachedThreadPool();
+        Future<Integer> future = executor.submit(new Callable<Integer>() {
+
+            @Override
+            public Integer call() {
+                Random random = new Random();
+                int duration = random.nextInt(4000);
+                System.out.println("Starting...");
+                try {
+                    Thread.sleep(duration);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("Finished");
+                return duration;
+            }
+        });
+
+        executor.shutdown();
+        
+        try {
+            System.out.println("Worked for: " + future.get());
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+}
